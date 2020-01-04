b0dcba5425164ec44397de662c81ccea0531e3db
jjangjamin <jaeminim95@gmail.com>
2020-01-04 11:26:08 +0900

    네이버 로그인 / 샘플 splash

---
+++ app/src/main/java/com/example/anandobby/LoadingActivity.java
@@ -0,0 +1,37 @@
+package com.example.anandobby;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent mainIntent = new Intent(LoadingActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+
+
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
+
+
