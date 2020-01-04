21862c8bed71e66bc44be0452d87c757b1e101e8
os94 <ss1511@naver.com>
2020-01-04 10:55:53 +0900

    Spring Boot 초기화

---
+++ server/src/main/java/com/fourman/anamdobby/AnamdobbyApplication.java
@@ -0,0 +1,13 @@
+package com.fourman.anamdobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnamdobbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnamdobbyApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/fourman/anamdobby/AnamdobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourman.anamdobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnamdobbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
