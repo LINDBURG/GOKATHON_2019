4968dbb72dfbcae7020bc6e45a40f837be191baf
os94 <ss1511@naver.com>
2020-01-04 14:47:24 +0900

    feat: TestController 수정

--- server/src/main/java/com/fourman/anamdobby/TestController.java
+++ server/src/main/java/com/fourman/anamdobby/TestController.java
@@ -1,18 +1,12 @@
 package com.fourman.anamdobby;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/")
+    @GetMapping("/test")
     String test() {
-        return "server running";
-    }
-
-    @GetMapping("/json")
-    ResponseEntity<String> test2() {
-        return ResponseEntity.ok("json data");
+        return "server connected";
     }
 }
